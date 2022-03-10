422189e3b87aba4fa046bcfb4e9474644bebea81
TH3_S1LENC3 <54558771+Th3-S1lenc3@users.noreply.github.com>
2022-03-10 18:31:55 +0000

    fix: add custom theme
    fix: add aliases for components and lib

--- svelte.config.js
+++ svelte.config.js
@@ -3,10 +3,19 @@ import sveltePreprocess from 'svelte-preprocess';
 import * as sass from 'sass';
 import tailwind from 'tailwindcss';
 import autoprefixer from 'autoprefixer';
+import path from 'path';
 
 export default {
 	kit: {
-		adapter: adapter()
+		adapter: adapter(),
+		vite: {
+			resolve: {
+				alias: {
+					'@components': path.resolve('./src/lib/components'),
+					'@lib': path.resolve('./src/lib')
+				}
+			}
+		}
 	},
 	preprocess: sveltePreprocess({
     sass: {
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,7 +1,32 @@
+const colors = require('tailwindcss/colors');
+
 module.exports = {
   mode: 'jit',
   content: ['./src/**/*.{js,svelte,html}'],
   darkMode: 'class',
-  theme: {},
+  theme: {
+    extend: {
+      colors: {
+        primary: colors.blue['800'],
+        secondary: '#444',
+        success: colors.green['600'],
+        info: colors.cyan['600'],
+        warning: colors.amber['500'],
+        danger: colors.red['600'],
+        light: '#7b8a8b',
+        dark: '#303030',
+        gray: {
+          900: '#202225',
+          800: '#2f3136',
+          700: '#36393f',
+          600: '#4f545c',
+          400: '#d4d7dc',
+          300: '#e3e5e8',
+          200: '#ebedef',
+          100: '#f2f3f5'
+        }
+      }
+    }
+  },
   plugins: []
 }
