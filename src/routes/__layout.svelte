36160b9c5b571f512448b2298dbe1ba53e6b111e
TH3_S1LENC3 <54558771+Th3-S1lenc3@users.noreply.github.com>
2022-03-10 18:37:55 +0000

    feat: add fontawesome webfonts
    fix: remove bootstrap icons

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -2,16 +2,24 @@
 </script>
 
 <style lang="scss" global>
+  @import-normalize;
   @tailwind base;
   @tailwind components;
   @tailwind utilities;
+
+  $fa-font-path: "/fonts"
+
+  @import "~@fortawesome/fontawesome-free/scss/fontawesome.scss";
+  @import "~@fortawesome/fontawesome-free/scss/brands.scss";
+  @import "~@fortawesome/fontawesome-free/scss/regular.scss";
+  @import "~@fortawesome/fontawesome-free/scss/solid.scss";
 </style>
 
 <header>
 
 </header>
 
-<main class="bg-dark flex-1">
+<main class="flex-1">
   <slot />
 </main>
 
