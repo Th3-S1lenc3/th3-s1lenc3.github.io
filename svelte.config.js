faad37b3fcf544630d2d6bf259a111af528a0662
TH3_S1LENC3 <54558771+Th3-S1lenc3@users.noreply.github.com>
2022-03-11 20:57:48 +0000

    feat: add api dependencies and config

--- svelte.config.js
+++ svelte.config.js
@@ -1,18 +1,19 @@
-import adapter from '@sveltejs/adapter-auto';
 import sveltePreprocess from 'svelte-preprocess';
 import * as sass from 'sass';
 import tailwind from 'tailwindcss';
 import autoprefixer from 'autoprefixer';
 import path from 'path';
+import node from '@sveltejs/adapter-node';
 
 export default {
 	kit: {
-		adapter: adapter(),
+		adapter: node(),
 		vite: {
 			resolve: {
 				alias: {
 					'@components': path.resolve('./src/lib/components'),
-					'@lib': path.resolve('./src/lib')
+					'@lib': path.resolve('./src/lib'),
+					'@api': path.resolve('./src/api')
 				}
 			}
 		}
